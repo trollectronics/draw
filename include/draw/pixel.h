a830fdcb19fb1a7ccdcad03fde0262adfbffb71f
Axel Isaksson <isaksson.axel@telia.com>
2021-09-19 02:07:25 +0200

    pixel draw color

--- include/draw/pixel.h
+++ include/draw/pixel.h
@@ -1,6 +1,9 @@
 #ifndef DRAW_PIXEL_H_
 #define DRAW_PIXEL_H_
 
+#include "color.h"
+
 void draw_pixel(int x, int y);
+void draw_pixel_color(int x, int y, DrawColor col);
 
 #endif
--- src/pixel.c
+++ src/pixel.c
@@ -6,3 +6,7 @@
 void draw_pixel(int x, int y) {
 	draw_framebuffer[y * DRAW_SCREEN_W + x] = draw_color;
 }
+
+void draw_pixel_color(int x, int y, DrawColor col) {
+	draw_framebuffer[y * DRAW_SCREEN_W + x] = col;
+}
